4df0e0ee233d0235f41c51fb68dc765a8d7be2ac
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2017-12-19 15:15:08 -0700

    Step 5 - create controller that can read swag and send it

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 require('dotenv').config();
+const checkForSession = require('./middlewares/checkForSession');
+const swagController = require('./controllers/swag_controller');
 
 const app = express();
 
+
 app.use( bodyParser.json() );
 app.use( session({
-    secret: process.env.SECRET_SESSION,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
 }));
+app.use( checkForSession );
+
+app.get('/api/swag', swagController.read);
 
 const port = process.env.PORT;
-app.listen(port, () => { console.log(`Listening on port: ${port}`) } );
+app.listen( port, () => { console.log(`Listening on port: ${port}`) } );
