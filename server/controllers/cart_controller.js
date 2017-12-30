a9b6807323960956706e2929c708dce12660a5dc
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2017-12-30 00:59:00 -0700

    Step 8 - Created cart controller and methods for adding and deleting

--- server/controllers/cart_controller.js
+++ server/controllers/cart_controller.js
@@ -1,15 +1,39 @@
 const swag = require('../models/swag');
 
 module.exports = {
-    add: (req, res, next) => {
-        req.body.id
-        res.status(200).send(session.user)
+    add: ( req, res, next ) => {
+        const { id } = req.query;
+        const { cart, total } = req.session.user;
+
+        const index = cart.findIndex( e => e.id == id );
+
+        if ( index === -1 ) {
+            const item = swag.find( e => e.id == id );
+
+            cart.push( item );
+            total += item.price;
+        }
+        
+        res.status(200).send( user );
     },   
-    remove: (req, res, next) => {
-        req.body.id
-        res.status(200).send(session.user)
+    remove: ( req, res, next ) => {
+        const { id } = req.query;
+        const { cart, total } = req.session.user;
+        
+        const item = swag.find( e => e.id == id );
+
+        if ( item ) {
+          const index = cart.findIndex( swag => swag.id == id );
+          cart.splice(index, 1);
+          total -= item.price;
+        }
+        
+        res.status(200).send( req.session.user );
     },   
-    checkout: (req, res, next) => {
-        res.status(200).send(session.user)
+    checkout: ( req, res, next ) => {
+        req.session.cart = [];
+        req.session.total = 0;
+
+        res.status(200).send( req.session.user );
     },   
 }
